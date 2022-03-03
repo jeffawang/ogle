fdcb485bb6e886d27a4d3f3d39c74413306de718
Jeff Wang <jeffawang@gmail.com>
2022-03-03 00:49:26 -0800

    add monaco-editor

--- src/app.tsx
+++ src/app.tsx
@@ -1,68 +1,103 @@
-import Ogle from './components/ogle';
-import REGL from 'regl';
+// import Ogle from './components/ogle';
+import REGL, { Regl } from 'regl';
 
 // @ts-ignore
 import vertex_shader from './shaders/shader.vert';
 // @ts-ignore
 import fragment_shader from './shaders/shader.frag';
 
-let cancel;
+import * as monaco from 'monaco-editor';
+import { threadId } from 'worker_threads';
+
 window.onload = function() {
 
   let canvas = document.getElementById("shader-view") as HTMLCanvasElement;
   let gl = canvas.getContext("webgl");
-
   const shader_tester = new ShaderTester(gl);
-  const [ok, err] = shader_tester.test(fragment_shader);
-  console.log(ok, err)
 
-  const regl = REGL(canvas);
-
-  const draw = regl({
-    frag: fragment_shader,
-    vert: vertex_shader,
-    attributes: {
-      position: [[-1, -1], [3, -1], [-1, 3]],
-      uv: [0, 0, 2, 0, 0, 2],
-    },
-    uniforms: {
-      u_time: ({time}) => time,
-      u_frame: ({tick}) => tick,
-    },
-    count: 3,
-
+  const editor = monaco.editor.create(/** @type {HTMLElement} */ (document.getElementById('monaco-editor')), {
+    value: fragment_shader,
+    language: 'glsl'
   });
+  editor.getModel().onDidChangeContent((event) => {
+    const value = editor.getValue();
+    const [ok, err] = shader_tester.test(value);
+    console.log(ok, err)
+  })
 
-{
-  // let draw2
-  // try {
-  //   draw2 = regl({
-  //     frag: fragment_shader + "jaowijef",
-  //     vert: vertex_shader,
-  //     attributes: {
-  //       position: [[-1, -1], [3, -1], [-1, 3]],
-  //       uv: [0, 0, 2, 0, 0, 2],
-  //     },
-  //     uniforms: {
-  //       u_time: ({time}) => time,
-  //       u_frame: ({tick}) => tick,
-  //     },
-  //     count: 3,
+  const ogle = new Ogle(canvas, fragment_shader, vertex_shader);
+  ogle.start();
 
-  //   });
-  // } catch (error) {
-  //   console.warn(error.stack);
-  // }
+  // const regl = REGL(canvas);
+
+  // const draw = regl({
+  //   frag: fragment_shader,
+  //   vert: vertex_shader,
+  //   attributes: {
+  //     position: [[-1, -1], [3, -1], [-1, 3]],
+  //     uv: [0, 0, 2, 0, 0, 2],
+  //   },
+  //   uniforms: {
+  //     u_time: ({time}) => time,
+  //     u_frame: ({tick}) => tick,
+  //   },
+  //   count: 3,
+
+  // });
+
+  // let cancel = regl.frame((context: REGL.DefaultContext) => {
+  //   regl.clear({
+  //     color: [0, 0, 0, 1],
+  //     depth: 1,
+  //   })
+  //   draw();
+  // })
 }
 
-  console.log("before")
-  cancel = regl.frame((context: REGL.DefaultContext) => {
-    regl.clear({
-      color: [0, 0, 0, 1],
-      depth: 1,
-    })
-    draw();
-  })
+class Ogle {
+  regl: REGL.Regl;
+  vert: string;
+  frag: string;
+  cmd: REGL.DrawCommand;
+
+  constructor(canvas: HTMLCanvasElement, frag: string, vert: string) {
+    this.regl = REGL(canvas);
+    this.frag = frag;
+    this.vert = vert;
+
+    this.set_frag(frag);
+  }
+
+  set_frag(frag: string) {
+    this.frag = frag;
+    this.cmd = this.regl({
+      frag: this.frag,
+      vert: this.vert,
+      attributes: {
+        position: [[-1, -1], [3, -1], [-1, 3]],
+        uv: [0, 0, 2, 0, 0, 2],
+      },
+      uniforms: {
+        u_time: ({time}) => time,
+        u_frame: ({tick}) => tick,
+      },
+      count: 3,
+    });
+  }
+
+  draw() {
+    this.cmd();
+  }
+
+  start() {
+    this.regl.frame((context: REGL.DefaultContext) => {
+      this.regl.clear({
+        color: [0, 0, 0, 1],
+        depth: 1,
+      })
+      this.cmd();
+    });
+  }
 }
 
 
--- src/shaders/shader.frag
+++ src/shaders/shader.frag
@@ -3,6 +3,6 @@ uniform int u_frame;
 uniform float u_time;
 varying vec2 v_uv;
 void main() {
-    gl_FragColor.rgb = vec2(0.8, 0.7, 1.0) + 0.3 * cos(v_uv.xyx + u_time);
+    gl_FragColor.rgb = vec3(0.8, 0.7, 1.0) + 0.3 * cos(v_uv.xyx + u_time);
     gl_FragColor.a = 1.0;
 }
